14003a40e9e38179d2a64cf2f51af7cd8989613c
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2023-05-10 22:18:59 -0300

    :white_check_mark: Ajustes nos testes e criacao da suite de testes.

--- src/test/java/br/com/pokedex/services/AttackServiceTest.java
+++ src/test/java/br/com/pokedex/services/AttackServiceTest.java
@@ -11,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.internal.matchers.Any;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
---
+++ src/test/java/br/com/pokedex/services/SuiteTests.java
@@ -0,0 +1,13 @@
+package br.com.pokedex.services;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AttackServiceTest.class,
+        PokeserviceTest.class
+})
+public class SuiteTests {
+    //Remova se puder;
+}
